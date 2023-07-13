6234d0ac7419806f939d7defaea5f1debc282993
Joseph Ita <odohofreh.joseph.i@gmail.com>
2023-07-13 18:06:20 +0000

    Print a C string with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	char MyStr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(MyStr);
+
+	return (0);
+}

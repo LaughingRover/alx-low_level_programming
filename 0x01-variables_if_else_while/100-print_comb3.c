8dd1932ed8f48619ac3aaadf0948b83ec14cabad
Joseph Ita <odohofreh.joseph.i@gmail.com>
2023-07-14 18:05:30 +0000

    Print different combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,5 +7,26 @@
  */
 int main(void)
 {
+	int c;
+	int d;
+
+	for (c = '0'; c <= '9';)
+	{
+		for (d = c + 1; d <= '9'; d++)
+		{
+			putchar(c);
+			putchar(d);
+
+			if (c != '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		c++;
+	}
+	putchar('\n');
+
 	return (0);
 }
+

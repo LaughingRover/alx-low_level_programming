4eb41c22d3bf304a22a4faca102108b68f5bd6bb
Joseph Ita <odohofreh.joseph.i@gmail.com>
2023-07-18 13:15:14 +0000

    print 10 times the alphabet with custom putchar

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,7 +1,13 @@
 #include <unistd.h>
 
+/**
+ * _putchar - prints characters to output
+ * @c: character to print (1 byte)
+ *
+ * Return: 1 (On success return number of bytes written)
+ */
 int _putchar(char c)
 {
-	return write(1, &c, 1);
+	return (write(1, &c, 1));
 }
 

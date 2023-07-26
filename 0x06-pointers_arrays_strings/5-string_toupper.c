8919c3c18c2f8a48ffb846559376eac1a9e24dac
Joseph Ita <odohofreh.joseph.i@gmail.com>
2023-07-26 21:02:51 +0000

    Change lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase letters of a string to uppercase
+ * @s: string
+ *
+ * Return: uppercase string
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+		i++;
+	}
+
+	return (s);
+}

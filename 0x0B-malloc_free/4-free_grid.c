c9e25712456336a0c4214d33b0dbf71041c9b34f
Joseph Ita <odohofreh.joseph.i@gmail.com>
2023-08-08 11:10:31 +0000

    Free allocated memory for 2D array

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2 dimensional grid created by alloc_grid function.
+ * @grid: 2D array
+ * @height: amount of rows
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
+

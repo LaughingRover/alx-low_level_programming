b6d2f8aa4932dfa9cce34be87eb1b62184895c5f
Joseph Ita <odohofreh.joseph.i@gmail.com>
2023-08-14 17:07:42 +0000

    Optimize print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,19 +13,8 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	if (d->name != NULL)
-		printf("Name: %s\n", d->name);
-	else
-		printf("(nil)\n");
-
-	if (d->age)
-		printf("Age: %f\n", d->age);
-	else
-		printf("(nil)\n");
-
-	if (d->owner != NULL)
-		printf("Owner: %s\n", d->owner);
-	else
-		printf("(nil)\n");
+	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+	printf("Age: %.1f\n", d->age);
+	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 }
 
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include "dog.h"
 
 /**
@@ -13,14 +14,14 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog = malloc(sizeof(new_dog));
+	dog_t *new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
 		return (NULL);
 
 	/*Allocate memory and store a copy of name and owner*/
-	new_dog->name = strdup(name);
-	new_dog->owner = strdup(owner);
+	new_dog->name = malloc(strlen(name) + 1);
+	new_dog->owner = malloc(strlen(owner) + 1);
 
 	if (new_dog->name == NULL || new_dog->owner == NULL)
 	{
@@ -31,6 +32,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
+	strcpy(new_dog->name, name);
+	strcpy(new_dog->owner, owner);
 	new_dog->age = age;
 
 	return (new_dog);

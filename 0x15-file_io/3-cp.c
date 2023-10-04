d7f720eea23cef570072d2b14e49d4a4c411d73d
Joseph Ita <odohofreh.joseph.i@gmail.com>
2023-10-04 19:54:58 +0000

    Amend File I/O cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,7 +12,7 @@ static char buffer[BUFFER_SIZE];
  */
 void exit_with_error(int code, const char *message, const char *filename)
 {
-	dprintf(STDERR_FILENO, "Error: %s %s\n", message, filename);
+	dprintf(STDERR_FILENO, "%s %s\n", message, filename);
 	exit(code);
 }
 
@@ -36,33 +36,33 @@ int main(int argc, char *argv[])
 	file_to = argv[2];
 	fd_src = open(file_from, O_RDONLY);
 	if (fd_src == -1)
-		exit_with_error(98, "Can't read from file %s", file_from);
+		exit_with_error(98, "Error: Can't read from file %s", file_from);
 
 	fd_dest = open(file_to, O_WRONLY | O_CREAT | O_TRUNC,
-		       S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (fd_dest == -1)
 	{
 		close(fd_src);
-		exit_with_error(99, "Can't write to %s", file_to);
+		exit_with_error(99, "Error: Can't write to %s", file_to);
 	}
 	while ((bytes_read = read(fd_src, buffer, sizeof(buffer))) > 0)
 	{
 		bytes_written = write(fd_dest, buffer, bytes_read);
-		if (bytes_written == -1)
+		if (bytes_written != bytes_read)
 		{
 			close(fd_src);
 			close(fd_dest);
-			exit_with_error(99, "Can't write to %s", file_to);
+			exit_with_error(99, "Error: Can't write to %s", file_to);
 		}
 	}
 	if (bytes_read == -1)
 	{
 		close(fd_src);
 		close(fd_dest);
-		exit_with_error(98, "Can't read from file %s", file_from);
+		exit_with_error(98, "Error: Can't read from file %s", file_from);
 	}
 	if (close(fd_src) == -1 || close(fd_dest) == -1)
-		exit_with_error(100, "Can't close fd %s",
-		(fd_src) ? file_from : file_to);
+		exit_with_error(100, "Error: Can't close fd %s",
+				(fd_src) ? file_from : file_to);
 	return (0);
 }
